998dde62ba70985e4d8ffd0b4407002def50dddc
IcaroWanderley <iwss.cin@ufpe.br>
2023-03-27 01:40:28 -0300

    questoes 25 -30

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
+
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  aluno: Aluno = {nome: "", cpf: "", email: ""};
-}
+  aluno: Aluno = {nome: "", cpf: "", email: "",};
+  alunoService = new AlunoService();
+  alunos: Aluno[] = []
+  cpfduplicado: boolean = false
 
-export class Aluno {
-  nome: string | undefined;
-  cpf: string | undefined;
-  email: string | undefined;
+  gravar(a: Aluno): void {
+    if (this.alunoService.gravar(a)) {
+      this.alunos.push(a);
+      this.aluno = {nome: "", cpf: "", email: ""};
+    } else {
+      this.cpfduplicado = true;
+    }
+ }
+ onmove(): void {
+  this.cpfduplicado = false;
+ }
 }
